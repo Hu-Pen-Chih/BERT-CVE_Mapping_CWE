2828ce04791c1cb463d1eff398eb2294bd50c47c
Hu-Pen-Chih <81350713+Hu-Pen-Chih@users.noreply.github.com>
2024-07-16 16:08:52 +0800

    Update Pretrain_for_MLM.py

--- Pretrain_for_MLM.py
+++ Pretrain_for_MLM.py
@@ -7,11 +7,10 @@ import warnings
 import logging
 import pandas as pd
 import os
-import pickle
 import random
 import numpy as np
 
-# 設置隨機數種子
+# Set random seed for reproducibility
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
@@ -21,12 +20,32 @@ def set_seed(seed):
 
 set_seed(42)
 
-
-# 隱藏tokenizer截斷的warning
+# Suppress specific warnings
 logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
 warnings.filterwarnings("ignore", message=".*overflowing tokens are not returned.*")
 
+# File paths and column names
+files_columns = {
+    'All_CWE.csv': 'CWE-Description',
+    #'Pretrain_data_200k.csv': 'CVE-Description',
+    'Pretrain_data_100k.csv': 'CVE-Description'
+}
 
+# Hyperparameters
+MAX_LENGTH = 512
+MASK_PROB = 0.15
+REPLACE_MASK_PROB = 0.8
+RANDOM_REPLACE_PROB = 0.1
+KEEP_ORIGINAL_PROB = 0.1
+BATCH_SIZE = 16
+NUM_EPOCHS = 5
+LEARNING_RATE = 5e-4
+BETA1 = 0.9
+BETA2 = 0.999
+WEIGHT_DECAY = 0.01
+EARLY_STOPPING_PATIENCE = 3
+
+# Read data function
 def read_data(file_path, column_name):
     try:
         df = pd.read_csv(file_path, encoding='utf-8')
@@ -36,33 +55,25 @@ def read_data(file_path, column_name):
         except UnicodeDecodeError:
             df = pd.read_csv(file_path, encoding='latin1')
     
-    # 查看文件
-    print(f"文件：{file_path}")
-    print("列名：", df.columns)
-    print("行數：", len(df))
+    # Display file info
+    print(f"File: {file_path}")
+    print("Columns:", df.columns)
+    print("Number of rows:", len(df))
 
-    return df[column_name].dropna().tolist()  # 移除任何空值
+    return df[column_name].dropna().tolist()  # Remove any NaN values
 
-# 文件路徑和欄位名稱
-files_columns = {
-    'All_CWE.csv': 'CWE-Description',
-    'Pretrain_data_200k.csv': 'CVE-Description',
-    'Pretrain_data_100k.csv':'CVE-Description'
-}
-
-# 載入數據
+# Load data
 texts = []
 for file_path, column_name in files_columns.items():
     texts.extend(read_data(file_path, column_name))
 
-# 確認讀取的文本數量
-print(f"共載入 {len(texts)} 篇文本。")
+# Confirm the number of loaded texts
+print(f"Total loaded texts: {len(texts)}")
 
-
-# 15% 的 token 會被隨機選擇進行遮蔽。在這些被選中的 token 中，80% 會被替換為 [MASK] 標記，10% 會被替換為隨機的其他 token，而剩下的 10% 則保持不變
-def create_masked_lm_inputs(texts, tokenizer, max_length=512, mask_prob=0.15, replace_mask_prob=0.8, random_replace_prob=0.10, keep_original_prob=0.10):
+# Create masked LM inputs
+def create_masked_lm_inputs(texts, tokenizer, max_length=MAX_LENGTH, mask_prob=MASK_PROB, replace_mask_prob=REPLACE_MASK_PROB, random_replace_prob=RANDOM_REPLACE_PROB, keep_original_prob=KEEP_ORIGINAL_PROB):
     pairs = []
-    for i, text in enumerate(texts):
+    for text in texts:
         inputs = tokenizer(
             text=text,
             add_special_tokens=True,
@@ -74,18 +85,18 @@ def create_masked_lm_inputs(texts, tokenizer, max_length=512, mask_prob=0.15, re
         input_ids = inputs['input_ids'][0]
         attention_mask = inputs['attention_mask'][0]
 
-        # 進行隨機遮蔽
+        # Random masking
         labels = input_ids.clone()
         probability_matrix = torch.full(labels.shape, mask_prob)
         special_tokens_mask = tokenizer.get_special_tokens_mask(labels.tolist(), already_has_special_tokens=True)
         probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
         masked_indices = torch.bernoulli(probability_matrix).bool()
 
-        # 替換為 [MASK]
+        # Replace with [MASK]
         indices_replaced = torch.bernoulli(torch.full(labels.shape, replace_mask_prob)).bool() & masked_indices
         input_ids[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
 
-        # 隨機替換
+        # Random replacement
         indices_random = torch.bernoulli(torch.full(labels.shape, random_replace_prob)).bool() & masked_indices & ~indices_replaced
         random_words = torch.randint(tokenizer.vocab_size, labels.shape, dtype=torch.long)
         valid_random_words = ~torch.tensor(tokenizer.get_special_tokens_mask(random_words.tolist(), already_has_special_tokens=True), dtype=torch.bool)
@@ -95,14 +106,12 @@ def create_masked_lm_inputs(texts, tokenizer, max_length=512, mask_prob=0.15, re
 
     return pairs
 
-
-# 初始化tokenizer
+# Initialize tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 pairs = create_masked_lm_inputs(texts, tokenizer)
-print(f"創建的MLM輸入數量: {len(pairs)}")
+print(f"Created MLM inputs: {len(pairs)}")
 
-
-# 創建數據集
+# Create dataset
 class CustomTextDataset(Dataset):
     def __init__(self, pairs):
         self.pairs = pairs
@@ -119,52 +128,43 @@ class CustomTextDataset(Dataset):
         }
 
 dataset = CustomTextDataset(pairs)
+print("Dataset created.")
 
-print("數據集創建完成。")
-
-# 分割數據集為訓練集和驗證集
+# Split dataset into training and validation sets
 train_size = int(0.8 * len(pairs))
 train_pairs = pairs[:train_size]
 eval_pairs = pairs[train_size:]
 
 train_dataset = CustomTextDataset(train_pairs)
 eval_dataset = CustomTextDataset(eval_pairs)
-print("數據集創建完成。")
+print("Train and validation datasets created.")
 
-batch_size = 16
+# Create data loaders
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+eval_loader = DataLoader(eval_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
-# 創建數據加載器
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-eval_loader = DataLoader(eval_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+# Set training parameters
+total_steps = NUM_EPOCHS * len(train_loader)
+warmup_steps = total_steps // 10  # Warm-up steps for learning rate
 
-# 設置訓練參數
-num_epochs = 20
-learning_rate = 2e-5
-beta1 = 0.9  # AdamW 的 Beta1
-beta2 = 0.999  # AdamW 的 Beta2
-weight_decay = 0.01  # 權重衰減
-total_steps = num_epochs * len(train_loader)
-warmup_steps = total_steps // 10  # 在訓練初期逐漸增加學習率的步數
-early_stopping_patience = 2  # 設置early stopping的耐心次數
-
-
-# 初始化 BERT MLM 模型
+# Initialize BERT MLM model
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-optimizer = AdamW(model.parameters(), lr=learning_rate, betas=(beta1, beta2), weight_decay=weight_decay, eps=1e-8)
+optimizer = AdamW(model.parameters(), lr=LEARNING_RATE, betas=(BETA1, BETA2), weight_decay=WEIGHT_DECAY, eps=1e-8)
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
 model.train()
-# 初始化最佳loss和early stopping
+
+# Initialize best loss and early stopping
 best_loss = float('inf')
 patience_counter = 0
 
-# 開始訓練並使用 tqdm 顯示訓練進度
-print("訓練開始。")
-for epoch in range(num_epochs):
+# Start training
+print("Training started.")
+for epoch in range(NUM_EPOCHS):
     model.train()
     epoch_loss = 0
-    print(f"Epoch {epoch + 1}/{num_epochs}")
+    print(f"Epoch {epoch + 1}/{NUM_EPOCHS}")
     epoch_iterator = tqdm(train_loader, desc=f"Epoch {epoch + 1}")
     for step, batch in enumerate(epoch_iterator):
         batch = {k: v.to(device) for k, v in batch.items()}
@@ -182,9 +182,9 @@ for epoch in range(num_epochs):
         epoch_loss += loss.item()
 
     avg_epoch_loss = epoch_loss / len(train_loader)
-    print(f"Epoch {epoch + 1} 平均訓練損失: {avg_epoch_loss}")
+    print(f"Epoch {epoch + 1} Average Training Loss: {avg_epoch_loss}")
 
-    # 計算驗證損失
+    # Evaluate
     model.eval()
     eval_loss = 0
     with torch.no_grad():
@@ -199,10 +199,10 @@ for epoch in range(num_epochs):
             eval_loss += loss.item()
     
     avg_eval_loss = eval_loss / len(eval_loader)
-    print(f"Epoch {epoch + 1} 平均驗證損失: {avg_eval_loss}")
+    print(f"Epoch {epoch + 1} Average Validation Loss: {avg_eval_loss}")
 
-    # 在每個時期結束時儲存模型
-    save_directory = f'pretrain_model_storage/pre-train-cve-2e-5-all-0714/epoch_{epoch + 1}'
+    # Save model at the end of each epoch
+    save_directory = f'pretrain_model_storage/best_pre-train-cve-2e-5-all-0714/epoch_{epoch + 1}'
     model_path = os.path.join(save_directory, 'pytorch_model.bin')
     os.makedirs(save_directory, exist_ok=True)
     torch.save(model.state_dict(), model_path)
@@ -210,17 +210,17 @@ for epoch in range(num_epochs):
 
     config_path = os.path.join(save_directory, 'config.json')
     model.config.to_json_file(config_path)
-    print(f"模型權重和配置已保存到 {model_path} 和 {config_path}")
+    print(f"Model weights and config saved to {model_path} and {config_path}")
 
-    # 檢查是否需要早停
+    # Check for early stopping
     if avg_eval_loss < best_loss:
         best_loss = avg_eval_loss
         patience_counter = 0
     else:
         patience_counter += 1
 
-    if patience_counter >= early_stopping_patience:
-        print(f"早停於Epoch {epoch + 1}")
+    if patience_counter >= EARLY_STOPPING_PATIENCE:
+        print(f"Early stopping at Epoch {epoch + 1}")
         break
 
-print("訓練完成。")
+print("Training completed.")
